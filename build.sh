ee45e94791894088ea2b6d13b78d2863dcd8a110
simpletoimplement <simpletoimplement@gmail.com>
2022-12-15 14:16:33 +0200

    feat: automate full build

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-chmod +x ./alpine-make-rootfs/alpine-make-rootfs
-sudo ./alpine-make-rootfs/alpine-make-rootfs --branch edge --timezone 'Europe/Tallinn' --script-chroot --packages 'apk-tools tzdata bash curl su-exec>=0.2 ca-certificates' rootfs.tar.gz || exit
-
